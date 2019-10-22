d0ff1020aa3e6ed5a0124615a28012db2c7be035
GYEONG DEOK <kims18@nate.com>
2019-10-22 22:04:52 +0900

    first commit

---
+++ project/mystorage/serializers.py
@@ -0,0 +1,31 @@
+from .models import Essay, Album, Files
+from rest_framework import serializers
+
+
+class EssaySerializer(serializers.ModelSerializer):
+
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Essay
+        fields = ('pk', 'title', 'body', 'author')
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+
+    author = serializers.ReadOnlyField(source='author.username')
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = Album
+        fields = ('pk', 'author', 'image', 'desc')
+
+
+class FileSerializer(serializers.ModelSerializer):
+
+    author = serializers.ReadOnlyField(source='author.username')
+    myfile = serializers.FileField(use_url=True)
+
+    class Meta:
+        model = Files
+        fields = ('pk', 'author', 'myfile', 'desc')
